a758e1a36dc9661aaad67db90e7d38789411ad8b
Aldiantoro Nugroho <aldi@burufly.com>
2013-02-07 13:26:15 +0700

    Updated.

--- bin/ps_mem.py
+++ bin/ps_mem.py
@@ -7,8 +7,9 @@
 # The shared RAM is problematic to calculate, and this script automatically
 # selects the most accurate method available for your kernel.
 
-# Author: P@draigBrady.com
-# Source: http://www.pixelbeat.org/scripts/ps_mem.py
+# Licence: LGPLv2
+# Author:  P@draigBrady.com
+# Source:  http://www.pixelbeat.org/scripts/ps_mem.py
 
 # V1.0      06 Jul 2005     Initial release
 # V1.1      11 Aug 2006     root permission required for accuracy
@@ -35,18 +36,8 @@
 #                           Patch from patrice.bouchand.fedora@gmail.com
 # V1.9      20 Feb 2008     Fix invalid values reported when PSS is available.
 #                           Reported by Andrey Borzenkov <arvidjaar@mail.ru>
-# V2.0      15 Jan 2010     From a report by Brock Noland <brockn@gmail.com>
-#                           about overreporting of RAM usage of his java progs,
-#                           handle linux clones that have pids. I.E. that have
-#                           CLONE_VM specified without CLONE_THREAD.
-# V2.1      20 Jan 2010     Append [deleted] or [updated] to programs which are
-#                           no longer on disk or have a new version available.
-#                           Add a --split-args option to group programs based
-#                           on the full command line, which could be used
-#                           to monitor separate "pmon" processes for example:
-#                             ps_mem.py | grep [p]mon
-# V2.2      16 Feb 2010     Support python 3.
-#                           Patch from Brian Harring <ferringb@gmail.com>
+# V3.0      01 Feb 2013
+#   http://github.com/pixelb/scripts/commits/master/scripts/ps_mem.py
 
 # Notes:
 #
@@ -78,8 +69,16 @@
 # I don't take account of memory allocated for a program
 # by other programs. For e.g. memory used in the X server for
 # a program could be determined, but is not.
+#
+# FreeBSD is supported if linprocfs is mounted at /compat/linux/proc/
+# FreeBSD 8.0 supports up to a level of Linux 2.6.16
 
-import sys, os, string
+import getopt
+import time
+import errno
+import os
+import sys
+
 try:
     # md5 module is deprecated on python 2.6
     # so try the newer hashlib first
@@ -89,74 +88,156 @@ except ImportError:
     import md5
     md5_new = md5.new
 
-if os.geteuid() != 0:
-    sys.stderr.write("Sorry, root permission required.\n");
-    sys.exit(1)
 
-split_args=False
-if len(sys.argv)==2 and sys.argv[1] == "--split-args":
-    split_args = True
+# The following exits cleanly on Ctrl-C or EPIPE
+# while treating other exceptions as before.
+def std_exceptions(etype, value, tb):
+    sys.excepthook = sys.__excepthook__
+    if issubclass(etype, KeyboardInterrupt):
+        pass
+    elif issubclass(etype, IOError) and value.errno == errno.EPIPE:
+        pass
+    else:
+        sys.__excepthook__(etype, value, tb)
+sys.excepthook = std_exceptions
 
-PAGESIZE=os.sysconf("SC_PAGE_SIZE")/1024 #KiB
-our_pid=os.getpid()
+#
+#   Define some global variables
+#
+
+PAGESIZE = os.sysconf("SC_PAGE_SIZE") / 1024 #KiB
+our_pid = os.getpid()
+
+have_pss = 0
+
+class Proc:
+    def __init__(self):
+        uname = os.uname()
+        if uname[0] == "FreeBSD":
+            self.proc = '/compat/linux/proc'
+        else:
+            self.proc = '/proc'
+
+    def path(self, *args):
+        return os.path.join(self.proc, *(str(a) for a in args))
+
+    def open(self, *args):
+        return open(self.path(*args))
+
+proc = Proc()
+
+
+#
+#   Functions
+#
+
+def parse_options():
+    try:
+        long_options = ['split-args', 'help']
+        opts, args = getopt.getopt(sys.argv[1:], "shp:w:", long_options)
+    except getopt.GetoptError:
+        sys.stderr.write(help())
+        sys.exit(3)
+
+    # ps_mem.py options
+    split_args = False
+    pids_to_show = None
+    watch = None
+
+    for o, a in opts:
+        if o in ('-s', '--split-args'):
+            split_args = True
+        if o in ('-h', '--help'):
+            sys.stdout.write(help())
+            sys.exit(0)
+        if o in ('-p',):
+            try:
+                pids_to_show = [int(x) for x in a.split(',')]
+            except:
+                sys.stderr.write(help())
+                sys.exit(3)
+        if o in ('-w',):
+            try:
+                watch = int(a)
+            except:
+                sys.stderr.write(help())
+                sys.exit(3)
+
+    return (split_args, pids_to_show, watch)
+
+def help():
+    help_msg = 'ps_mem.py - Show process memory usage\n'\
+    '\n'\
+    '-h                                 Show this help\n'\
+    '-w <N>                             Measure and show process memory every N seconds\n'\
+    '-p <pid>[,pid2,...pidN]            Only show memory usage PIDs in the specified list\n'
+
+    return help_msg
 
 #(major,minor,release)
 def kernel_ver():
-    kv=open("/proc/sys/kernel/osrelease", "rt").readline().split(".")[:3]
+    kv = proc.open('sys/kernel/osrelease').readline().split(".")[:3]
+    last = len(kv)
+    if last == 2:
+        kv.append('0')
+    last -= 1
     for char in "-_":
-        kv[2]=kv[2].split(char)[0]
+        kv[last] = kv[last].split(char)[0]
+    try:
+        int(kv[last])
+    except:
+        kv[last] = 0
     return (int(kv[0]), int(kv[1]), int(kv[2]))
 
-kv=kernel_ver()
-
-have_pss=0
 
 #return Private,Shared
 #Note shared is always a subset of rss (trs is not always)
 def getMemStats(pid):
     global have_pss
     mem_id = pid #unique
-    Private_lines=[]
-    Shared_lines=[]
-    Pss_lines=[]
-    Rss=int(open("/proc/"+str(pid)+"/statm", "rt").readline().split()[1])*PAGESIZE
-    if os.path.exists("/proc/"+str(pid)+"/smaps"): #stat
+    Private_lines = []
+    Shared_lines = []
+    Pss_lines = []
+    Rss = (int(proc.open(pid, 'statm').readline().split()[1])
+           * PAGESIZE)
+    if os.path.exists(proc.path(pid, 'smaps')): #stat
         digester = md5_new()
-        for line in open("/proc/"+str(pid)+"/smaps", "rb").readlines(): #open
+        for line in proc.open(pid, 'smaps').readlines(): #open
             # Note we checksum smaps as maps is usually but
             # not always different for separate processes.
-            digester.update(line)
-            line = line.decode("ascii")
+            digester.update(line.encode('latin1'))
             if line.startswith("Shared"):
                 Shared_lines.append(line)
             elif line.startswith("Private"):
                 Private_lines.append(line)
             elif line.startswith("Pss"):
-                have_pss=1
+                have_pss = 1
                 Pss_lines.append(line)
         mem_id = digester.hexdigest()
-        Shared=sum([int(line.split()[1]) for line in Shared_lines])
-        Private=sum([int(line.split()[1]) for line in Private_lines])
+        Shared = sum([int(line.split()[1]) for line in Shared_lines])
+        Private = sum([int(line.split()[1]) for line in Private_lines])
         #Note Shared + Private = Rss above
         #The Rss in smaps includes video card mem etc.
         if have_pss:
-            pss_adjust=0.5 #add 0.5KiB as this average error due to trunctation
-            Pss=sum([float(line.split()[1])+pss_adjust for line in Pss_lines])
+            pss_adjust = 0.5 # add 0.5KiB as this avg error due to trunctation
+            Pss = sum([float(line.split()[1])+pss_adjust for line in Pss_lines])
             Shared = Pss - Private
-    elif (2,6,1) <= kv <= (2,6,9):
-        Shared=0 #lots of overestimation, but what can we do?
+    elif (2,6,1) <= kernel_ver() <= (2,6,9):
+        Shared = 0 #lots of overestimation, but what can we do?
         Private = Rss
     else:
-        Shared=int(open("/proc/"+str(pid)+"/statm", "rt").readline().split()[2])
-        Shared*=PAGESIZE
+        Shared = int(proc.open(pid, 'statm').readline().split()[2])
+        Shared *= PAGESIZE
         Private = Rss - Shared
     return (Private, Shared, mem_id)
 
-def getCmdName(pid):
-    cmdline = open("/proc/%d/cmdline" % pid, "rt").read().split("\0")
+
+def getCmdName(pid, split_args):
+    cmdline = proc.open(pid, 'cmdline').read().split("\0")
     if cmdline[-1] == '' and len(cmdline) > 1:
         cmdline = cmdline[:-1]
-    path = os.path.realpath("/proc/%d/exe" % pid) #exception for kernel threads
+    path = proc.path(pid, 'exe')
+    path = os.path.realpath(path) #exception for kernel threads
     if split_args:
         return " ".join(cmdline)
     if path.endswith(" (deleted)"):
@@ -172,95 +253,31 @@ def getCmdName(pid):
             else:
                 path += " [deleted]"
     exe = os.path.basename(path)
-    cmd = open("/proc/%d/status" % pid, "rt").readline()[6:-1]
+    cmd = proc.open(pid, 'status').readline()[6:-1]
     if exe.startswith(cmd):
-        cmd=exe #show non truncated version
+        cmd = exe #show non truncated version
         #Note because we show the non truncated name
         #one can have separated programs as follows:
         #584.0 KiB +   1.0 MiB =   1.6 MiB    mozilla-thunder (exe -> bash)
         # 56.0 MiB +  22.2 MiB =  78.2 MiB    mozilla-thunderbird-bin
     return cmd
 
-cmds={}
-shareds={}
-mem_ids={}
-count={}
-for pid in os.listdir("/proc/"):
-    if not pid.isdigit():
-        continue
-    pid = int(pid)
-    if pid == our_pid:
-        continue
-    try:
-        cmd = getCmdName(pid)
-    except:
-        #permission denied or
-        #kernel threads don't have exe links or
-        #process gone
-        continue
-    try:
-        private, shared, mem_id = getMemStats(pid)
-    except:
-        continue #process gone
-    if shareds.get(cmd):
-        if have_pss: #add shared portion of PSS together
-            shareds[cmd]+=shared
-        elif shareds[cmd] < shared: #just take largest shared val
-            shareds[cmd]=shared
-    else:
-        shareds[cmd]=shared
-    cmds[cmd]=cmds.setdefault(cmd,0)+private
-    if cmd in count:
-       count[cmd] += 1
-    else:
-       count[cmd] = 1
-    mem_ids.setdefault(cmd,{}).update({mem_id:None})
-
-#Add shared mem for each program
-total=0
-for cmd in cmds:
-    cmd_count = count[cmd]
-    if len(mem_ids[cmd]) == 1 and cmd_count > 1:
-        # Assume this program is using CLONE_VM without CLONE_THREAD
-        # so only account for one of the processes
-        cmds[cmd] /= cmd_count
-        if have_pss:
-            shareds[cmd] /= cmd_count
-    cmds[cmd]=cmds[cmd]+shareds[cmd]
-    total+=cmds[cmd] #valid if PSS available
-
-if sys.version_info >= (2, 6):
-    sort_list = sorted(cmds.items(), key=lambda x:x[1])
-else:
-    sort_list = cmds.items()
-    sort_list.sort(lambda x,y:cmp(x[1],y[1]))
-# list wrapping is redundant on <py3k, needed for >=pyk3 however
-sort_list=list(filter(lambda x:x[1],sort_list)) #get rid of zero sized processes
 
 #The following matches "du -h" output
 #see also human.py
 def human(num, power="Ki"):
-    powers=["Ki","Mi","Gi","Ti"]
+    powers = ["Ki", "Mi", "Gi", "Ti"]
     while num >= 1000: #4 digits
         num /= 1024.0
-        power=powers[powers.index(power)+1]
-    return "%.1f %s" % (num,power)
+        power = powers[powers.index(power)+1]
+    return "%.1f %s" % (num, power)
+
 
 def cmd_with_count(cmd, count):
-    if count>1:
-       return "%s (%u)" % (cmd, count)
+    if count > 1:
+        return "%s (%u)" % (cmd, count)
     else:
-       return cmd
-
-sys.stdout.write(" Private  +   Shared  =  RAM used\tProgram \n\n")
-for cmd in sort_list:
-    sys.stdout.write("%8sB + %8sB = %8sB\t%s\n" % (human(cmd[1]-shareds[cmd[0]]),
-                                      human(shareds[cmd[0]]), human(cmd[1]),
-                                      cmd_with_count(cmd[0], count[cmd[0]])))
-if have_pss:
-    sys.stdout.write("%s\n%s%8sB\n%s\n" % ("-" * 33,
-        " " * 24, human(total), "=" * 33))
-sys.stdout.write("\n Private  +   Shared  =  RAM used\tProgram \n\n")
+        return cmd
 
 #Warn of possible inaccuracies
 #2 = accurate & can total
@@ -269,39 +286,170 @@ sys.stdout.write("\n Private  +   Shared  =  RAM used\tProgram \n\n")
 #-1= all shared mem not reported
 def shared_val_accuracy():
     """http://wiki.apache.org/spamassassin/TopSharedMemoryBug"""
+    kv = kernel_ver()
     if kv[:2] == (2,4):
-        if open("/proc/meminfo", "rt").read().find("Inact_") == -1:
+        if proc.open('meminfo').read().find("Inact_") == -1:
             return 1
         return 0
     elif kv[:2] == (2,6):
-        if os.path.exists("/proc/"+str(os.getpid())+"/smaps"):
-            if open("/proc/"+str(os.getpid())+"/smaps", "rt").read().find("Pss:")!=-1:
+        pid = os.getpid()
+        if os.path.exists(proc.path(pid, 'smaps')):
+            if proc.open(pid, 'smaps').read().find("Pss:")!=-1:
                 return 2
             else:
                 return 1
         if (2,6,1) <= kv <= (2,6,9):
             return -1
         return 0
+    elif kv[0] > 2:
+        return 2
     else:
         return 1
 
-vm_accuracy = shared_val_accuracy()
-if vm_accuracy == -1:
-    sys.stderr.write(
-     "Warning: Shared memory is not reported by this system.\n"
-    )
-    sys.stderr.write(
-     "Values reported will be too large, and totals are not reported\n"
-    )
-elif vm_accuracy == 0:
-    sys.stderr.write(
-     "Warning: Shared memory is not reported accurately by this system.\n"
-    )
-    sys.stderr.write(
-     "Values reported could be too large, and totals are not reported\n"
-    )
-elif vm_accuracy == 1:
-    sys.stderr.write(
-     "Warning: Shared memory is slightly over-estimated by this system\n"
-     "for each program, so totals are not reported.\n"
-    )
+def show_shared_val_accuracy( possible_inacc ):
+    if possible_inacc == -1:
+        sys.stderr.write(
+         "Warning: Shared memory is not reported by this system.\n"
+        )
+        sys.stderr.write(
+         "Values reported will be too large, and totals are not reported\n"
+        )
+    elif possible_inacc == 0:
+        sys.stderr.write(
+         "Warning: Shared memory is not reported accurately by this system.\n"
+        )
+        sys.stderr.write(
+         "Values reported could be too large, and totals are not reported\n"
+        )
+    elif possible_inacc == 1:
+        sys.stderr.write(
+         "Warning: Shared memory is slightly over-estimated by this system\n"
+         "for each program, so totals are not reported.\n"
+        )
+    sys.stderr.close()
+
+def get_memory_usage( pids_to_show, split_args, include_self=False, only_self=False ):
+    cmds = {}
+    shareds = {}
+    mem_ids = {}
+    count = {}
+    for pid in os.listdir(proc.path('')):
+        if not pid.isdigit():
+            continue
+        pid = int(pid)
+
+        # Some filters
+        if only_self and pid != our_pid:
+            continue
+        if pid == our_pid and not include_self:
+            continue
+        if pids_to_show is not None and pid not in pids_to_show:
+            continue
+
+        try:
+            cmd = getCmdName(pid, split_args)
+        except:
+            #permission denied or
+            #kernel threads don't have exe links or
+            #process gone
+            continue
+
+        try:
+            private, shared, mem_id = getMemStats(pid)
+        except RuntimeError:
+            continue #process gone
+        if shareds.get(cmd):
+            if have_pss: #add shared portion of PSS together
+                shareds[cmd] += shared
+            elif shareds[cmd] < shared: #just take largest shared val
+                shareds[cmd] = shared
+        else:
+            shareds[cmd] = shared
+        cmds[cmd] = cmds.setdefault(cmd, 0) + private
+        if cmd in count:
+            count[cmd] += 1
+        else:
+            count[cmd] = 1
+        mem_ids.setdefault(cmd, {}).update({mem_id:None})
+
+    #Add shared mem for each program
+    total = 0
+    for cmd in cmds:
+        cmd_count = count[cmd]
+        if len(mem_ids[cmd]) == 1 and cmd_count > 1:
+            # Assume this program is using CLONE_VM without CLONE_THREAD
+            # so only account for one of the processes
+            cmds[cmd] /= cmd_count
+            if have_pss:
+                shareds[cmd] /= cmd_count
+        cmds[cmd] = cmds[cmd] + shareds[cmd]
+        total += cmds[cmd] #valid if PSS available
+
+    sorted_cmds = sorted(cmds.items(), key=lambda x:x[1])
+    sorted_cmds = [x for x in sorted_cmds if x[1]]
+
+    return sorted_cmds, shareds, count, total
+
+def print_header():
+    sys.stdout.write(" Private  +   Shared  =  RAM used\tProgram \n\n")
+
+def print_memory_usage(sorted_cmds, shareds, count, total):
+    for cmd in sorted_cmds:
+        sys.stdout.write("%8sB + %8sB = %8sB\t%s\n" %
+                         (human(cmd[1]-shareds[cmd[0]]),
+                          human(shareds[cmd[0]]), human(cmd[1]),
+                          cmd_with_count(cmd[0], count[cmd[0]])))
+    if have_pss:
+        sys.stdout.write("%s\n%s%8sB\n%s\n" %
+                         ("-" * 33, " " * 24, human(total), "=" * 33))
+
+def verify_environment():
+    if os.geteuid() != 0:
+        sys.stderr.write("Sorry, root permission required.\n")
+        if __name__ == '__main__':
+            sys.stderr.close()
+            sys.exit(1)
+
+    try:
+        kv = kernel_ver()
+    except (IOError, OSError):
+        val = sys.exc_info()[1]
+        if val.errno == errno.ENOENT:
+            sys.stderr.write(
+              "Couldn't access " + proc.path('') + "\n"
+              "Only GNU/Linux and FreeBSD (with linprocfs) are supported\n")
+            sys.exit(2)
+        else:
+            raise
+
+if __name__ == '__main__':
+    verify_environment()
+    split_args, pids_to_show, watch = parse_options()
+
+    print_header()
+
+    if watch is not None:
+        try:
+            sorted_cmds = True
+            while sorted_cmds:
+                sorted_cmds, shareds, count, total = get_memory_usage( pids_to_show, split_args )
+                print_memory_usage(sorted_cmds, shareds, count, total)
+                time.sleep(watch)
+            else:
+                sys.stdout.write('Process does not exist anymore.\n')
+        except KeyboardInterrupt:
+            pass
+    else:
+        # This is the default behavior
+        sorted_cmds, shareds, count, total = get_memory_usage( pids_to_show, split_args )
+        print_memory_usage(sorted_cmds, shareds, count, total)
+
+
+    # We must close explicitly, so that any EPIPE exception
+    # is handled by our excepthook, rather than the default
+    # one which is reenabled after this script finishes.
+    sys.stdout.close()
+
+    vm_accuracy = shared_val_accuracy()
+    show_shared_val_accuracy( vm_accuracy )
+
